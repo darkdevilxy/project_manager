6e03c995273289dc8be46c6ab9f4c8034b65851a
darkdevilxy <bhumagainxy@gmail.com>
2022-11-09 21:31:43 +1100

    Style Changes - Added a new style for the "New" and "Edit" button on the main page - Changed the display layout for the project list - Overall UI Changes for better user experience

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,8 +1,10 @@
-import { collection, doc,deleteDoc, getFirestore } from 'firebase/firestore';
+import { collection, doc, deleteDoc, getFirestore } from 'firebase/firestore';
 import { useFirestoreCollectionData } from 'reactfire';
 
 import edit from './../../assets/edit.svg'
 import add from './../../assets/plus.svg'
+import edit_unfilled from './../../assets/pencil_unfilled.svg'
+import add_unfilled from './../../assets/add_not_filled.svg'
 
 import './sidebar.css'
 
@@ -13,19 +15,21 @@ import { useState } from 'react';
 
 export default function Sidebar() {
 
-    const db_ref = collection(getFirestore(), 'projects');
-    const { status, data: projects } = useFirestoreCollectionData(db_ref, { idField: 'id' });
-    const [popup_state, set_popup_state] = useState(false) 
-    
-    function create_record() {
-        set_popup_state(()=> !popup_state)
 
+    const db_ref = collection( getFirestore(), 'projects' );
+    const { status, data: projects } = useFirestoreCollectionData( db_ref, { idField: 'id' } );
+    const [popup_state, set_popup_state] = useState( false )
+
+    const [edit_icon, set_edit_icon] = useState( edit )
+    const [add_icon, set_add_icon] = useState( add )
+
+    function create_record() {
+        set_popup_state( () => !popup_state )
     }
 
-    async function delete_record(name) {
-        const document = projects.find(item => item.project_name == name)
-        await deleteDoc(doc(db_ref, document.id))
-
+    async function delete_record( name ) {
+        const document = projects.find( item => item.project_name == name )
+        await deleteDoc( doc( db_ref, document.id ) )
     }
 
     return (
@@ -35,27 +39,54 @@ export default function Sidebar() {
                     Projects
                 </div>
                 <div className="top_section">
-                    <button onClick={create_record} className="new">
-                        <img src={add} alt="Add" />
+                    <button onMouseOver={
+                            () => set_add_icon( add_unfilled )
+                        }
+                        onMouseLeave={
+                            () => set_add_icon( add )
+                        }
+                        onClick={create_record}
+                        className="new">
+                        <img src={add_icon}
+                            alt="Add"/>
                     </button>
-                    <button onClick={() => delete_record("Test2")} className="edit">
-                        <img src={edit} alt="" />
+                    <button onMouseOver={
+                            () => set_edit_icon( edit_unfilled )
+                        }
+                        onMouseLeave={
+                            () => set_edit_icon( edit )
+                        }
+                        onClick={
+                            () => delete_record( "Test2" )
+                        }
+                        className="edit">
+                        <img src={edit_icon}
+                            alt=""/>
                     </button>
                 </div>
                 <div className="content">
                     {
-                        projects && projects.map(project => (
-                            <Smallcards name={project.project_name} key={project.id}/>
-                        ))
-                    }
-
-                </div>
+                    projects && projects.map( project => (
+                        <Smallcards name={
+                                project.project_name
+                            }
+                            key={
+                                project.id
+                            }/>
+                    ) )
+                } </div>
                 <div className="bottom_section">
                     <span>Status: {status}</span>
                 </div>
             </aside>
-            {console.log(popup_state)}
-            {popup_state? <Popup database={db_ref} status={set_popup_state}></Popup> : ""}
-        </>
+            {
+            console.log( popup_state )
+        }
+            {
+            popup_state ? <Popup database={db_ref}
+                status={set_popup_state}></Popup> : ""
+        } </>
     )
+
+
 }
