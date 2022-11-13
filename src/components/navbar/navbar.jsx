3fa7682db263e8d0152a14a58f7bad89c04f1033
darkdevilxy <bhumagainxy@gmail.com>
2022-11-13 23:21:10 +1100

    Major Changes: -UI Change 	Seacrh bar was made interactive and react onChange action was added to it 	Hamburger menu Icon was added 	Added effects on icons 		Profile Icon and other icons in navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,26 +1,30 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
+import { useState } from 'react'
 
 import { ProfilePhoto } from './profile_photo/profile_photo';
 
-import logo from './../../assets/home.svg'
+import home from './../../assets/home.svg'
+import hamburger from './../../assets/hamburger.svg'
 import './navbar.css'
 
 export function Navbar() {
+    const [search_text, set_search_text] = useState('');
 
     function authenticate_user(){
         const provider = new GoogleAuthProvider();
         const auth = getAuth();
         signInWithPopup(auth, provider)
     }
+    console.log(search_text)
 
     return (
         <nav className="navbar">
-            <div className="logo">
-                <img src={logo} alt="logo" />
+            <div className="left_section">
+                <img src={hamburger} alt="" className="hamburger_menu" />
+                <img src={home} alt="logo" />
             </div>
             <div className="search">
-                <img src="" alt="" className="search_icon" />
-                <input className='search_bar' type="text" />
+                <input value={search_text} onChange={(e)=> set_search_text(e.target.value)} className='search_bar' type="text" />
             </div>
             <div onClick={authenticate_user} className="user">
                 <ProfilePhoto />
