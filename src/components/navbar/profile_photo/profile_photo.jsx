f898dde60598e41ecd2be39e1117b2bfc02aa454
darkdevilxy <bhumagainxy@gmail.com>
2022-11-14 12:57:57 +1100

    Changes: -Chnged the conditional rendering method used to display user profile picture was changes to useEffect hook

--- src/components/navbar/profile_photo/profile_photo.jsx
+++ src/components/navbar/profile_photo/profile_photo.jsx
@@ -1,16 +1,24 @@
-import { useSigninCheck } from 'reactfire';
+import { useEffect, useState } from 'react';
+import { useSigninCheck, useUser } from 'reactfire';
 
-import profile_picture from './../../../assets/user (1).svg'
+import photo from './../../../assets/user (1).svg'
 
 import './profile_photo.css'
 
 export function ProfilePhoto(){
     const {status, data: user} = useSigninCheck();
-    const default_tempelate = <img src={profile_picture} alt="" className="profile_photo" />
-    if (status === 'loading') {
-    return (
-        default_tempelate
-    )
+    const {user_status, data:user_data} = useUser();
+    const [photo_url, set_photo_url] = useState();
+useEffect(()=>{
+    if (status !== 'loading' && user_status !== 'loading'){
+        if (user.signedIn){
+            set_photo_url(user_data.photoURL)
+
+        }
     }
-   return user.signedIn? <img src={user.user.photoURL} alt="" className="profile_photo_signed_in" /> : default_tempelate
+},[
+    status, user, user_data, user_status
+])
+
+return <img src={photo_url} alt="" className="profile_photo_signed_in" />
 }
